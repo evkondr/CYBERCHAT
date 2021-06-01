5b182991f4561f24644d0386ed85a74d14680bff
Evgeniy Kondratyev <kondratiev.evg@gmail.com>
2021-06-02 00:11:26 +0300

    mongoose installed, connected to mongo DB

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const http = require('http')
+const mongoose = require('mongoose')
 const server = http.createServer(app)
 const { Server } = require('socket.io')
 const authRouter = require('./routes/authRoute')
+const config = require('./config')
 const io = new Server(server, {
     cors: {
         origin: "http://localhost:9000",
@@ -24,6 +26,21 @@ io.on('connection', (socket) => {
     })
 
   });
+  async function connectDB(){
+    try{
+        await mongoose.connect(config.dbURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true
+          });
+          console.log('DB connected')
+    }catch(e){
+        console.log(e)
+    }
+    
+}
+connectDB()
 server.listen(3000, () => {
     console.log('Server is running on port 3000')
 })
